aef21e18720ec8ac7d07450e1c17149fb968cbb7
StanislavRadkov <Stanislav.vr@gmail.com>
2016-09-27 14:01:32 +0300

    - added solution to balanced brackets problem

---
+++ HackerRank/Stacks/BalancedBrackets.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stacks
+{
+    /// https://www.hackerrank.com/challenges/balanced-brackets
+    public static class BalancedBrackets
+    {
+        public static bool ValiedateBrackets(string input)
+        {
+            var brackets = input.ToCharArray();
+            var stack = new Stack<Char>();
+
+            if (brackets.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            var openingBrackets = new[] { '(','{','[' };
+            var closingBrackets = new[] { ')', '}', ']'};
+
+            foreach (var bracket in brackets)
+            {
+                if (openingBrackets.Contains(bracket))
+                {
+                    stack.Push(bracket);
+                }
+                else
+                {
+                    if (stack.Count > 0 && Array.IndexOf(closingBrackets, bracket) != Array.IndexOf(openingBrackets, stack.Pop()))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ HackerRank/StacksTests/BalancedBracketsTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stacks;
+
+namespace StacksTests
+{
+    [TestClass]
+    public class BalancedBracketsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("{[()]}"), true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("{[(])}"), false);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("{{[[(())]]}}"), true);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("}][}}(}][))]"), false);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("[](){()}"), true);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("()"), true);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("({}([][]))[]()"), true);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.AreEqual(BalancedBrackets.ValiedateBrackets("{)[](}]}]}))}(())("), false);
+        }
+    }
+}
