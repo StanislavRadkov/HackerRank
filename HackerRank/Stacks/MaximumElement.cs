e5282f327e082f2489c30fdd7212629d24ef4e74
StanislavRadkov <Stanislav.vr@gmail.com>
2016-09-27 13:07:23 +0300

    Initial commit

---
+++ HackerRank/Stacks/MaximumElement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    public class MaximumElement
+    {
+        private Stack<int> stack;
+        private Stack<int> maxStack;
+
+        public MaximumElement()
+        {
+            stack = new Stack<int>();
+            maxStack = new Stack<int>();
+        }
+        
+        public int? Parse(int[] query)
+        {
+            switch (query[0])
+            {
+                case 1:
+                    stack.Push(query[1]);
+                    if (maxStack.Count == 0 || (maxStack.Count > 0 && query[1] >= maxStack.Peek()))
+                    {
+                        maxStack.Push(query[1]);
+                    }
+                    break;
+
+                case 2:
+                    var value = stack.Pop();
+                    if (value == maxStack.Peek())
+                    {
+                        maxStack.Pop();
+                    }
+                    break;
+
+                case 3:
+                    return maxStack.Peek();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HackerRank/StacksTests/MaximumElementTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stacks;
+
+namespace StacksTests
+{
+    [TestClass]
+    public class MaximumElementTests
+    {
+        private MaximumElement element;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            element = new MaximumElement();
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            var queries = new[]
+                {
+                    new int[] {1, 97},
+                    new int[] {2},
+                    new int[] {1, 20},
+                    new int[] {2},
+                    new int[] {1, 26},
+                    new int[] {1, 20},
+                    new int[] {2},
+                    new int[] {3},
+                    new int[] {1, 91},
+                    new int[] {3}
+                };
+
+            var expectedOutput = new Stack<int>(new int[]{91, 26});
+
+            foreach (var query in queries)
+            {
+                var result = element.Parse(query);
+                if (result.HasValue)
+                {
+                    Assert.AreEqual(expectedOutput.Pop(), result);
+                }
+            }
+        }
+    }
+}
