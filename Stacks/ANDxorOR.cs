90fd98a624cdd5d0acc6c4cb3b6a811bc108e8dd
StanislavRadkov <Stanislav.vr@gmail.com>
2016-10-03 18:52:04 +0300

    initial implementation of AND xor OR, timeouts on most tests, needs optimization

---
+++ Stacks/ANDxorOR.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks
+{
+    // https://www.hackerrank.com/challenges/and-xor-or
+    public static class ANDxorOR
+    {
+        public static int Solve(List<int> data)
+        {
+            Func<int, int, int> calc = (m1, m2) => ((m1 & m2) ^ (m1 | m2)) & (m1 ^ m2);
+
+            var maxValue = 0;
+
+            for (var i = 0; i < data.Count - 1; i++)
+            {
+                for (var k = i + 1; k < data.Count; k++)
+                {
+                    int? a = null;
+                    int? b = null;
+
+                    for (var j = i; j <= k; j++)
+                    {
+                        if (b == null || data[j] < b)
+                        {
+                            a = b;
+                            b = data[j];
+                        }
+                        else if (a == null || data[j] < a)
+                        {
+                            a = data[j];
+                        }
+                    }
+
+                    var value = calc(a.Value, b.Value);
+
+                    if (value > maxValue)
+                    {
+                        maxValue = value;
+                    }
+                }
+            }
+
+            return maxValue;
+        }
+    }
+}
---
+++ StacksTests/ANDxorORTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stacks;
+
+namespace StacksTests
+{
+    [TestClass]
+    public class ANDxorORTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new List<int>() {9, 6, 3, 5, 2};
+
+            Assert.AreEqual(15, ANDxorOR.Solve(input));
+        }
+    }
+}
