597ad361f13ae237ea13e4dc3bc81d1e673b2e7a
StanislavRadkov <Stanislav.vr@gmail.com>
2016-10-14 16:50:45 +0300

    - Added solution to the kangaroo problem

---
+++ Implementation/Kangaroo.cs
@@ -0,0 +1,17 @@
+﻿namespace Implementation
+{
+    public static class Kangaroo
+    {
+        public static bool Solve(int x1, int v1, int x2, int v2)
+        {
+            if (v1 <= v2)
+            {
+                return false;
+            }
+            else
+            {
+                return (x2 - x1) % (v1 - v2) == 0;
+            }
+        }
+    }
+}
---
+++ ImplementationTests/KangarooTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class KangarooTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, Kangaroo.Solve(0, 3, 4, 2));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, Kangaroo.Solve(0, 2, 5, 3));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(false, Kangaroo.Solve(43, 2, 70, 2));
+        }
+    }
+}
