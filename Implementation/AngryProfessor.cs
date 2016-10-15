62d4cb0c23e018f3d43e06f77bf8ba4010a579d4
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-10-15 16:26:27 +0300

    Solution to the Angry professor task

---
+++ Implementation/AngryProfessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/angry-professor
+    public static class AngryProfessor
+    {
+        public static bool IsClassCanceled(int limit, int[] times)
+        {
+            return times.Count(t => t <= 0) < limit;
+        }
+    }
+}
---
+++ ImplementationTests/AngryProfessorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class AngryProfessorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, AngryProfessor.IsClassCanceled(3, new int[] { -1, -3, 4, 3 }));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, AngryProfessor.IsClassCanceled(2, new int[] { 0, -1, 2, 1 }));
+        }
+    }
+}
