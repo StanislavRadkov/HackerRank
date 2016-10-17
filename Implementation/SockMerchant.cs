54cf255ec9e101bccb01d69d9094842ca78c106e
StanislavRadkov <Stanislav.vr@gmail.com>
2016-10-17 13:27:33 +0300

    Solution to the sock merchant problem

---
+++ Implementation/SockMerchant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/sock-merchant
+    public static class SockMerchant
+    {
+        public static int Solve(int[] socks)
+        {
+            var count = 0;
+            var pairs = new HashSet<int>();
+
+            foreach (var sock in socks)
+            {
+                if (pairs.Contains(sock))
+                {
+                    count++;
+                    pairs.Remove(sock);
+                }
+                else
+                {
+                    pairs.Add(sock);
+                }
+            }
+
+            return count;
+        }
+    }
+}
