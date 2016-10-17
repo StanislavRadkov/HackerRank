8a4479a8166086859c47569fafc9922029a7cd21
StanislavRadkov <Stanislav.vr@gmail.com>
2016-10-17 13:19:53 +0300

    Solution to the cut the sticks problem

---
+++ Implementation/CutTheSticks.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/cut-the-sticks
+    public static class CutTheSticks
+    {
+        public static List<int> Solve(List<int> sticks)
+        {
+            var lengths = new List<int>() {sticks.Count};
+            var minValue = 0;
+            var newMinValue = sticks.Min();
+
+            while (sticks.Any())
+            {
+                minValue = newMinValue;
+                newMinValue = int.MaxValue;
+
+                for (var i = sticks.Count - 1; i >= 0; i--)
+                {
+                    sticks[i] -= minValue;
+                    if (sticks[i] <= 0)
+                    {
+                        sticks.RemoveAt(i);
+                    }
+                    else
+                    {
+                        if (sticks[i] < newMinValue)
+                        {
+                            newMinValue = sticks[i];
+                        }
+                    }
+                }
+
+                if (sticks.Count > 0)
+                {
+                    lengths.Add(sticks.Count);
+                }
+            }
+
+            return lengths;
+        }
+    }
+}
