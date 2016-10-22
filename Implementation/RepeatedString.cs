f24f43dcec33767d5dcc4410de2541746d311cad
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-10-22 16:41:06 +0300

    Solution to the repeated string solution

---
+++ Implementation/RepeatedString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/repeated-string
+    public static class RepeatedString
+    {
+        public static long Solve(string a, long n)
+        {
+            checked
+            {
+                Func<char, bool> f = c => c == 'a';
+
+                return a.Count(f) * (n/a.Length) + a.Substring(0, (int)(n % a.Length)).Count(f);
+            }
+        }
+    }
+}
---
+++ ImplementationTests/RepeatedStringTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class RepeatedStringTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(7, RepeatedString.Solve("aba", 10));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(16481469408, RepeatedString.Solve("epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq", 549382313570));
+        }
+    }
+}
