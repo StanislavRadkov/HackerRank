c64bed1a77f212ee0b8b421e9788ad12509b5be3
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-10-22 17:24:06 +0300

    - Added solution to the jumping on clouds problem

---
+++ Implementation/JumpingOnTheClouds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/jumping-on-the-clouds
+    public static class JumpingOnTheClouds
+    {
+        public static int Solve(bool[] clouds)
+        {
+            var steps = 0;
+
+            for (var i = 0; i < clouds.Length - 1; ) 
+            {
+                if (i + 2 < clouds.Length && clouds[i + 2] == false)
+                {
+                    steps++;
+                    i += 2;
+                }
+                else if (i + 1 < clouds.Length && clouds[i + 1] == false)
+                {
+                    steps++;
+                    i += 1;
+                }
+            }
+
+            return steps;
+        }
+    }
+}
---
+++ ImplementationTests/JumpingOnTheCloudsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Implementation;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class JumpingOnTheCloudsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, JumpingOnTheClouds.Solve(Array.ConvertAll(new int[] { 0, 0, 1, 0, 0, 1, 0 }, Convert.ToBoolean).ToArray()));
+        }
+    }
+}
