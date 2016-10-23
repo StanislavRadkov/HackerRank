8c0d442d48cfe63c0ce0aefe4a8971dfdf371597
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-10-23 15:04:09 +0300

    Added solution to the strange counter problem

---
+++ Implementation/StrangeCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/strange-code
+    public static class StrangeCounter
+    {
+        public static long Solve(long t) 
+        {
+            checked
+            {
+                long n = 1;
+                while (3 * (n - 1) < t) 
+                {
+                    n = 2 * n;
+                };
+
+                return (3 * (n - 1) - t + 1);
+            }
+        }
+    }
+}
---
+++ ImplementationTests/StrangeCounterTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class StrangeCounterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6, StrangeCounter.Solve(4));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, StrangeCounter.Solve(9));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(12, StrangeCounter.Solve(10));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, StrangeCounter.Solve(21));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(24, StrangeCounter.Solve(22));
+        }
+
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(649267441662, StrangeCounter.Solve(1000000000000));
+        }
+    }
+}
