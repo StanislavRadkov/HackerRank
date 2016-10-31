b5fcfd1f2ed6d726456071cda5b3bb2d8f7a3e35
StanislavRadkov <Stanislav.vr@gmail.com>
2016-10-31 16:20:45 +0200

    - Added bit manipulation index

---
+++ BitManipulation/LonelyInteger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitManipulation
+{
+    // https://www.hackerrank.com/challenges/lonely-integer
+    public static class LonelyInteger
+    {
+        public static int Solve(int[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BitManipulationTests/LonelyIntegerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitManipulation;
+
+namespace BitManipulationTests
+{
+    [TestClass]
+    public class LonelyIntegerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, LonelyInteger.Solve(new int[] { 1 }));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, LonelyInteger.Solve(new int[] { 1, 1, 2 }));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(2, LonelyInteger.Solve(new int[] { 0, 0, 1, 2, 1 }));
+        }
+    }
+}
