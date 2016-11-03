d327e8416297aeded964d337b91224dcb1b66372
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-11-03 21:40:45 +0200

    Solution to the maximizing xor problem

---
+++ BitManipulation/MaximizingXOR.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitManipulation
+{
+    // https://www.hackerrank.com/challenges/maximizing-xor
+    public static class MaximizingXOR
+    {
+        public static int Solve(int l, int r)
+        {
+            var max = -1;
+
+            for (var a = l; a <= r - 1; a++)
+            {
+                for (var b = a + 1; b <= r; b++)
+                {
+                    var value = a ^ b;
+                    if (value > max)
+                    {
+                        max = value;
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ BitManipulationTests/MaximizingXORTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BitManipulation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitManipulationTests
+{
+    [TestClass]
+    public class MaximizingXORTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(7, MaximizingXOR.Solve(10, 15));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(3, MaximizingXOR.Solve(5, 6));
+        }
+    }
+}
