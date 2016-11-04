a8054cb62a9500989c72235fcf97c89123a79e71
StanislavRadkov <Stanislav.vr@gmail.com>
2016-11-04 17:29:23 +0200

    Solution to the counter game problem

---
+++ BitManipulation/CounterGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BitManipulation
+{
+    // https://www.hackerrank.com/challenges/counter-game
+    public static class CounterGame
+    {
+        public static string Solve(UInt64 n)
+        {
+            var counter = 0;
+
+            while (n > 1)
+            {                
+                // If N is a power of 2, reduce the counter by half of N.
+                if ((n & (n-1)) == 0)
+                {
+                    n = n >> 1;
+                }
+                else // If N is not a power of 2, reduce the counter by the largest power of 2 less than N.
+                {
+                    var k = 63;
+                    while ((n & (1UL << k)) == 0)
+                    {
+                        k--;
+                    }
+
+                    n &= ~(1UL << k);
+                }
+
+                counter++;
+            }
+
+            return counter % 2 == 0 ? "Richard" : "Louise";
+        }
+    }
+}
---
+++ BitManipulationTests/CounterGameTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitManipulation;
+
+namespace BitManipulationTests
+{
+    [TestClass]
+    public class CounterGameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Richard", CounterGame.Solve(6));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("Richard", CounterGame.Solve(6703734870638684097));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("Richard", CounterGame.Solve(7597026128958891522));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual("Louise", CounterGame.Solve(13174607262084689114));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual("Louise", CounterGame.Solve(6959712971461184279));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual("Louise", CounterGame.Solve(12572864054437627553));
+        }
+    }
+}
