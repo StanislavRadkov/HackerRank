0d3260738611cf73cd97818b33bcd7af0b9caf6d
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-11-07 21:17:55 +0200

    Solution to the flipping bits problem

---
+++ BitManipulation/FlippingBits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitManipulation
+{
+    public static class FlippingBits
+    {
+        public static uint[] Solve(uint[] input)
+        {
+            var numbers = input.Clone() as uint[];
+            for (var n = 0; n < numbers.Length; n++)
+            {
+                numbers[n] = ~numbers[n];
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ BitManipulationTests/FlippingBitsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitManipulation;
+
+namespace BitManipulationTests
+{
+    [TestClass]
+    public class FlippingBitsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new uint[] { 2147483648, 4294967294, 4294967295 }, FlippingBits.Solve(new uint[] { 2147483647, 1, 0}));
+        }
+    }
+}
