e5c2b0bd7a4b916f74c40b104017369dcca36c18
StanislavRadkov <Stanislav.vr@gmail.com>
2016-11-22 12:37:50 +0200

    Added sorting project

---
+++ Sorting/IntrotoTutorialChallenges.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sorting
+{
+    public static class IntroToTutorialChallenges
+    {
+        public static int Solve(int[] arr, int k)
+        {
+            return Array.IndexOf(arr, k);
+        }
+    }
+}
---
+++ SortingTests/IntroToTutorialChallengesTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorting;
+
+namespace SortingTests
+{
+    [TestClass]
+    public class IntroToTutorialChallengesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, IntroToTutorialChallenges.Solve(new int[] { 1, 4, 5, 7, 9, 12 }, 4));
+        }
+    }
+}
