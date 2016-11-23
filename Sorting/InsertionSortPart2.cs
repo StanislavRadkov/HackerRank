de4534c0e0f57e19bb98a6eb7b708824379fff41
StanislavRadkov <Stanislav.vr@gmail.com>
2016-11-23 17:52:43 +0200

    Added solution to insertion sort 2

---
+++ Sorting/InsertionSortPart2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    // https://www.hackerrank.com/challenges/insertionsort2
+    public static class InsertionSortPart2
+    {
+        public static IEnumerable<int[]> Sort(int[] data)
+        {
+            for (var i = 1; i < data.Length; i++)
+            {
+                var x = data[i];
+                var j = i - 1;
+
+                while (j >= 0 && data[j] > x)
+                {
+                    data[j + 1] = data[j];
+                    j--;
+                }
+
+                data[j + 1] = x;
+
+                int[] result = new int[data.Length];
+                Array.Copy(data, result, data.Length);
+
+                yield return result;
+            }
+        }
+    }
+}
---
+++ SortingTests/InsertionSortPart2Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace SortingTests
+{    
+    [TestClass]
+    public class InsertionSortPart2Tests
+    {       
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expected = new[] 
+            {
+                new int[] {1, 4, 3, 5, 6, 2 },
+                new int[] {1, 3, 4, 5, 6, 2 },
+                new int[] {1, 3, 4, 5, 6, 2 },
+                new int[] {1, 3, 4, 5, 6, 2 },
+                new int[] {1, 2, 3, 4, 5, 6 }
+            };
+
+            var output = InsertionSortPart2.Sort(new int[] { 1, 4, 3, 5, 6, 2 }).ToArray();
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i]);
+            }
+        }
+    }
+}
