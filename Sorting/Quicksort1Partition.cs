e9bb5ce96cf09ca31b943d6b4b4c9bace6323086
StanislavRadkov <Stanislav.vr@gmail.com>
2016-12-01 13:05:09 +0200

    Added solution to the Quicksort2 problem

---
+++ Sorting/Quicksort1Partition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Sorting
+{
+    // https://www.hackerrank.com/challenges/quicksort2
+    public static class Quicksort1Partition
+    {
+        public static List<int> Partion(int[] data)
+        {
+            var pivot = data[0];
+            var equal = new List<int>();
+            var left = new List<int>();
+            var right = new List<int>();
+
+            foreach (var value in data)
+            {
+                if (value < pivot)
+                {
+                    left.Add(value);
+                }
+                else if (value > pivot)
+                {
+                    right.Add(value);
+                }
+                else
+                {
+                    equal.Add(value);
+                }
+            }
+
+            left.AddRange(equal);
+            left.AddRange(right);
+
+            return left;
+        }
+    }
+}
---
+++ SortingTests/Quicksort1PartitionTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortingTests
+{
+    [TestClass]
+    public class Quicksort1PartitionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new int[] { 3, 2, 4, 5, 7 }, Quicksort1Partition.Partion(new int[] { 4, 5, 3, 7, 2 }));
+        }
+    }
+}
