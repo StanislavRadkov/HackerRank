d03c7c35a5cab5566cd3648986845a5687b9539d
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-12-24 15:19:22 +0200

    Added Queue project

---
+++ Queues/QueueUsingTwoStacks.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Queues
+{
+    // https://www.hackerrank.com/challenges/queue-using-two-stacks
+    public class QueueUsingTwoStacks<T>
+    {
+        private Stack<T> stack1;
+        private Stack<T> stack2;
+
+        public QueueUsingTwoStacks()
+            : this(1024)
+        {
+            
+        }
+
+        public QueueUsingTwoStacks(int size)
+        {
+            stack1 = new Stack<T>(size);
+            stack2 = new Stack<T>(size);
+        }
+
+        public void Enqueue(T item)
+        {
+            stack2.Push(item);
+        }
+
+        public T Dequeue()
+        {
+            if (!stack1.Any())
+            {
+                while (stack2.Any())
+                {
+                    stack1.Push(stack2.Pop());
+                }
+            }
+
+            return stack1.Pop();
+        }
+
+        public T Peek()
+        {
+            if (!stack1.Any())
+            {
+                while (stack2.Any())
+                {
+                    stack1.Push(stack2.Pop());
+                }
+            }
+
+            return stack1.Peek();
+        }
+    }
+}
---
+++ QueuesTests/QueueUsingTwoStacksTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Queues;
+
+namespace QueuesTests
+{
+    [TestClass]
+    public class QueueUsingTwoStacksTests
+    {
+        private static QueueUsingTwoStacks<int>_queue ;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _queue = new QueueUsingTwoStacks<int>();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _queue.Enqueue(1);
+            _queue.Enqueue(2);
+            _queue.Enqueue(3);
+
+            Assert.AreEqual(1, _queue.Dequeue());
+            Assert.AreEqual(2, _queue.Dequeue());
+            Assert.AreEqual(3, _queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // using will make sure the file is closed
+            using (System.IO.StreamReader input = new System.IO.StreamReader(@"..\..\QueueUsingTwoStacksData\\input1.txt"))
+            {
+                using (System.IO.StreamReader output = new System.IO.StreamReader(@"..\..\QueueUsingTwoStacksData\\output1.txt"))
+                {
+                    var count = int.Parse(input.ReadLine());
+
+                    for(var i = 0; i < count; i++)
+                    {
+                        var commands = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+                        switch (commands[0])
+                        {
+                            case 1:
+                                _queue.Enqueue(commands[1]);
+                                break;
+
+                            case 2:
+                                _queue.Dequeue();
+                                break;
+
+                            case 3:
+                                Assert.AreEqual(int.Parse(output.ReadLine()), _queue.Peek());
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        //[TestMethod]
+        //[ExpectedException(typeof(InvalidOperationException))]
+        //public void TestMethod2()
+        //{
+        //    _queue.Dequeue();
+        //}
+    }
+}
