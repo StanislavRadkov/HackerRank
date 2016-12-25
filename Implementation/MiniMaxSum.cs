96a5d0649ca8c7bc959ac6f8513bfb1b993a1a06
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-12-25 10:14:48 +0200

    Added solution for mini max sum

---
+++ Implementation/MiniMaxSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/mini-max-sum
+    public static class MiniMaxSum
+    {
+        public static long[] Solve(long[] data)
+        {
+            Array.Sort(data);
+
+            var min = data.Take(4).Sum();
+            var max = data.Skip(1).Take(4).Sum();
+
+            return new long[] { min, max };
+        }
+    }
+}
---
+++ ImplementationTests/MiniMaxSumTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class MiniMaxSumTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new long[] { 10, 14 }, MiniMaxSum.Solve(new long[] { 1, 2, 3, 4, 5 }));
+        }
+    }
+}
