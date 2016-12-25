482d288e515dda6b04ec3d9ed62f2bbca4178643
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-12-25 11:54:09 +0200

    Sollution to the append and delete task

---
+++ Implementation/AppendAndDelete.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/append-and-delete
+    public static class AppendAndDelete
+    {
+        public static bool CanBeConverted(string a, string b, int k)
+        {
+            var index = IndexOfFirstDifference(a, b);
+
+            if (a.Length == b.Length)
+            {
+                if (index == -1)
+                {
+                    return true;
+                }
+            }
+            else if (a.Length != b.Length && index == -1)
+            {
+                var diff = Math.Abs(a.Length - b.Length);
+
+                if (k < diff)
+                {
+                    return false;
+                }
+
+                if (k > a.Length + b.Length)
+                {
+                    return true;
+                }
+
+                return (k - diff) % 2 == 0;
+            }
+
+            return ((a.Length - index) + (b.Length - index)) <= k;
+        }
+
+        private static int IndexOfFirstDifference(string a, string b)
+        {
+            var index = -1;
+            var minLength = Math.Min(a.Length, b.Length);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if(a[i] != b[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ ImplementationTests/AppendAndDeleteTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class AppendAndDeleteTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, AppendAndDelete.CanBeConverted("hackerhappy", "hackerrank", 9));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(true, AppendAndDelete.CanBeConverted("abc", "abc", 7));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(true, AppendAndDelete.CanBeConverted("aaaaaaaaaa", "aaaaa", 7));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(false, AppendAndDelete.CanBeConverted("y", "yu", 2));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(true, AppendAndDelete.CanBeConverted("aaa", "a", 5));
+        }
+    }
+}
