7ef1173e6a040a5c34e96a21f3bfafd3b97d241c
Stanislav Radkov <Stanislav.vr@gmail.com>
2016-12-25 10:29:50 +0200

    Added designer pdf task solution

---
+++ Implementation/DesignerPDFViewer.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/designer-pdf-viewer
+    public static class DesignerPDFViewer
+    {
+        public static int Solve(int[] heights, string word)
+        {
+            var maxHeight = Encoding.ASCII.GetBytes(word).Select(v => heights[v - 97]).Max();
+
+            return maxHeight * word.Length;
+        }
+    }
+}
---
+++ ImplementationTests/DesignerPDFViewerTests.cs
@@ -0,0 +1,15 @@
+﻿using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class DesignerPDFViewerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(9, DesignerPDFViewer.Solve(new int[] { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 }, "abc" ));
+        }
+    }
+}
