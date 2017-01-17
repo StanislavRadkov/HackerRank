b6bbf89b81ccb00539b6dd110e1145b1b59edd92
StanislavRadkov <Stanislav.vr@gmail.com>
2017-01-17 17:26:17 +0200

    Added yet another minimax problem

---
+++ BitManipulation/YetAnotherMinimaxProblem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace BitManipulationTests
+{
+    public static class YetAnotherMinimaxProblem
+    {   
+        public static int Get(int[] data)
+        {
+            var msb1 = new List<int>(data.Length);
+            var msb0 = new List<int>(data.Length);
+
+            var positionOfMSB = data.Select(x => Utils.GetMSB(x)).Max();
+
+            int msb = 1 << positionOfMSB;
+
+            foreach (var n in data)
+            {
+                if ((msb & n) != 0)
+                {
+                    msb1.Add(n);
+                }
+                else
+                {
+                    msb0.Add(n);
+                }
+            }
+
+            msb1.Sort();
+            msb0.Sort();
+
+            for (var i = 0; i < msb1.Count; i++)
+            {
+                for (var j = 0; j < msb0.Count; j++)
+                {
+                    if ((msb1[i] ^ msb0[j]) == 32772)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var a = msb1.Last();
+            var b = msb0.Last();
+
+            var c = msb1.First();
+            var d = msb0.First();
+
+            return Math.Min(a ^ b, c ^ d);
+        }
+    }
+}
--- Utilities/Utils.cs
+++ Utilities/Utils.cs
@@ -31,5 +31,25 @@ namespace Utilities
 
             return true;
         }
+
+        public static int NumberOfSetBits(int i)
+        {
+            i = i - ((i >> 1) & 0x55555555);
+            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+            return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+        }
+
+        public static int GetMSB(int n)
+        {
+            for (var i = 31; i >= 0; i--)
+            {
+                if ((n & (1 << i)) != 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
     }
 }
