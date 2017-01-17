bec3f8d04ebbfd31b75af765a07b53672a6a6a5e
StanislavRadkov <Stanislav.vr@gmail.com>
2017-01-17 12:51:35 +0200

    Added array left rotation problem

---
+++ CrackingTheCodingInterviewTests/ArraysLeftRotationTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrackingTheCodingInterview;
+
+namespace CrackingTheCodingInterviewTests
+{
+    [TestClass]
+    public class ArraysLeftRotationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("5 1 2 3 4", ArraysLeftRotation.RotateLeft(new int[] { 1, 2, 3, 4, 5 }, new int[] { 5, 4 }).ToString());
+        }
+    }
+}
