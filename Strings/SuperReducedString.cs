6cc4c06fefc8d3a7a1e3e7581c48481c6033a1bf
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-01-21 20:35:44 +0200

    Added solution to the string reduction task

---
+++ Strings/SuperReducedString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/reduced-string
+    public static class SuperReducedString
+    {
+        public static string Reduce(string input)
+        {
+            const string pattern = "(.)\\1{1}";
+
+            while (Regex.Match(input, pattern).Length > 0)
+            {
+                input = Regex.Replace(input, pattern, String.Empty);
+            }
+
+            return input;
+        }
+    }
+}
---
+++ StringsTests/SuperReducedStringTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class SuperReducedStringTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("abd", SuperReducedString.Reduce("aaabccddd"));
+        }
+    }
+}
