9847e04ae74e31c4fa4efb7e19d73de1a1b69904
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-01-21 20:46:22 +0200

    Added solution to the camel case problem

---
+++ Strings/Camelcase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/camelcase
+    public static class Camelcase
+    {
+        public static int CountWords(string input)
+        {
+            if (input.Length == 0)
+            {
+                return 0;
+            }
+
+            return Regex.Matches(input, "[A-Z]").Count + 1;
+        }
+    }
+}
---
+++ StringsTests/CamelcaseTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class CamelcaseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, Camelcase.CountWords("saveChangesInTheEditor"));
+        }
+    }
+}
