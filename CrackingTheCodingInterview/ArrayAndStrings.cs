167187cca5a571748888c7492a1538959541e1d6
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-01-27 11:01:40 +0200

    Added CTCI problem 1

---
+++ CrackingTheCodingInterview/ArrayAndStrings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    public static class ArrayAndStrings
+    {
+        public static bool AllUniqueCharactersASCII(string a)
+        {
+            var chars = new bool[256];
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (chars[a[i]])
+                {
+                    return false;
+                }
+
+                chars[a[i]] = true;
+            }
+
+            return true;
+        }
+
+        public static bool AllUniqueCharactersUnicode(string a)
+        {
+            var hashSet = new HashSet<char>();
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (hashSet.Contains(a[i]))
+                {
+                    return false;
+                }
+
+                hashSet.Add(a[i]);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CrackingTheCodingInterviewTests/ArrayAndStringsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrackingTheCodingInterview;
+
+namespace CrackingTheCodingInterviewTests
+{
+    [TestClass]
+    public class ArrayAndStringsTests
+    {
+        [TestMethod]
+        public void AllUniqueCharactersASCIITest()
+        {
+            Assert.AreEqual(true, ArrayAndStrings.AllUniqueCharactersASCII("abcd"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersASCII("abcdc"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersASCII("aa"));
+            Assert.AreEqual(true, ArrayAndStrings.AllUniqueCharactersASCII("a"));
+            Assert.AreEqual(true, ArrayAndStrings.AllUniqueCharactersASCII(""));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersASCII("kjsadasjdaaaa"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersASCII("anbhyrioplkkkkxxxx"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersASCII("aqwertyuio00"));
+        }
+
+        [TestMethod]
+        public void AllUniqueCharactersUnicodeTest()
+        {
+            Assert.AreEqual(true, ArrayAndStrings.AllUniqueCharactersUnicode("абцд"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersUnicode("абцдц"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersUnicode("аа"));
+            Assert.AreEqual(true, ArrayAndStrings.AllUniqueCharactersUnicode("а"));
+            Assert.AreEqual(true, ArrayAndStrings.AllUniqueCharactersUnicode(""));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersUnicode("касдяветрзз"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersUnicode("асдфгхйкльь"));
+            Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersUnicode("асдфгхйкл00"));
+        }
+    }
+}
