f5f354e383dbbea9c08907ced80352c9f7114b15
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-01-27 23:05:27 +0200

    Added array and strings problems and solutions

--- CrackingTheCodingInterview/ArrayAndStrings.cs
+++ CrackingTheCodingInterview/ArrayAndStrings.cs
@@ -41,5 +41,90 @@ namespace CrackingTheCodingInterview
 
             return true;
         }
+
+        public static string ReverseString(string a)
+        {
+            var input = a.ToCharArray();
+            char temp;
+
+            for (var i = 0; i < input.Length/2; i++)
+            {
+                temp = input[i];
+                input[i] = input[input.Length - 1 - i];
+                input[input.Length - 1 - i] = temp;
+            }
+
+            return String.Join(String.Empty, input);
+        }
+
+        public static bool IsPermutation(string a, string b)
+        {
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+
+            var chars1 = a.ToCharArray();
+            var chars2 = b.ToCharArray();
+
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+
+            for (var i = 0; i < chars1.Length; i++)
+            {
+                if (chars1[i] != chars2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static string ReplaceSpaces(string a)
+        {
+            return a.Trim().Replace(" ", "%20");
+        }
+
+        public static string CompressString(string a)
+        {
+            var sb = new StringBuilder(a.Length);
+
+            for (var i = 0; i < a.Length;)
+            {
+                var c = a[i];
+                var s = 1;
+                while (++i < a.Length && a[i] == c)
+                {
+                    s++;
+                }
+
+                sb.Append(String.Format("{0}{1}", c, s));
+            }
+
+            return sb.Length < a.Length ? sb.ToString() : a;
+        }
+
+        public static void RotateMatrix(int[][] matrix)
+        {
+            var n = matrix.Length;
+            int temp;
+
+            for (var row = 0; row < n/2; row++)
+            {
+                for (var column = 0; column < n/2; column++)
+                {
+                    temp = matrix[row][column];
+
+                    matrix[row][column] = matrix[n - column - 1][row];
+
+                    matrix[n - column - 1][row] = matrix[n - row - 1][n - column - 1];
+
+                    matrix[n - row - 1][n - column - 1] = matrix[column][n - row - 1];
+
+                    matrix[column][n - row - 1] = temp;
+                }
+            }
+        }
     }
 }
--- CrackingTheCodingInterviewTests/ArrayAndStringsTests.cs
+++ CrackingTheCodingInterviewTests/ArrayAndStringsTests.cs
@@ -32,5 +32,64 @@ namespace CrackingTheCodingInterviewTests
             Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersUnicode("асдфгхйкльь"));
             Assert.AreEqual(false, ArrayAndStrings.AllUniqueCharactersUnicode("асдфгхйкл00"));
         }
+
+        [TestMethod]
+        public void ReverseStringTest()
+        {
+            Assert.AreEqual("dcba", ArrayAndStrings.ReverseString("abcd"));
+            Assert.AreEqual("", ArrayAndStrings.ReverseString(""));
+            Assert.AreEqual("edcba", ArrayAndStrings.ReverseString("abcde"));
+        }
+
+        [TestMethod]
+        public void IsPermutationTests()
+        {
+            Assert.AreEqual(true, ArrayAndStrings.IsPermutation("abcd", "dcba"));
+            Assert.AreEqual(true, ArrayAndStrings.IsPermutation("apple", "papel"));
+            Assert.AreEqual(true, ArrayAndStrings.IsPermutation("carrot", "tarroc"));
+            Assert.AreEqual(false, ArrayAndStrings.IsPermutation("hello", "llloh"));
+            Assert.AreEqual(true, ArrayAndStrings.IsPermutation("", ""));
+        }
+
+        [TestMethod]
+        public void ReplaceSpacesTest()
+        {
+            Assert.AreEqual("Mr%20John%20Smith", ArrayAndStrings.ReplaceSpaces("Mr John Smith"));
+        }
+
+        [TestMethod]
+        public void CompressStringTests()
+        {
+            Assert.AreEqual("a2b1c5a3", ArrayAndStrings.CompressString("aabcccccaaa"));
+            Assert.AreEqual("", ArrayAndStrings.CompressString(""));
+            Assert.AreEqual("abc", ArrayAndStrings.CompressString("abc"));
+            Assert.AreEqual("44332211", ArrayAndStrings.CompressString("4444333221"));
+        }
+
+        [TestMethod]
+        public void RotateMatrixTests()
+        {
+            var matrix =  new int[][]
+            {
+                new []{1, 2, 3, 4},
+                new []{5, 6, 7, 8},
+                new []{9, 10, 11, 12},
+                new []{13, 14, 15, 16}
+            };
+
+            ArrayAndStrings.RotateMatrix(matrix);
+
+            var expected = new int[][]
+            {
+                new []{13, 9, 5, 1},
+                new []{14, 10, 6, 2},
+                new []{15, 11, 7, 3},
+                new []{16, 12, 8, 4}
+            };
+            for (var row = 0; row < matrix.Length; row++)
+            {
+                CollectionAssert.AreEqual(expected[row], matrix[row]);
+            }
+        }
     }
 }
