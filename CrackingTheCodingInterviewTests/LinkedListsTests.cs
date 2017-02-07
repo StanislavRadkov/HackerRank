b4cb406ac22d783787e1e8e961c6183646c5538f
StanislavRadkov <Stanislav.vr@gmail.com>
2017-02-07 17:07:38 +0200

    Added linked list tests

---
+++ CrackingTheCodingInterviewTests/LinkedListsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingTheCodingInterviewTests
+{
+    [TestClass]
+    public class LinkedListsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
