0c8cc23f5e02148e04fc1cb514b56a0a7fdbbf25
StanislavRadkov <Stanislav.vr@gmail.com>
2017-02-15 23:01:44 +0200

    Added stacks and queues

---
+++ CrackingTheCodingInterviewTests/StacksAndQueuesTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingTheCodingInterviewTests
+{
+    [TestClass]
+    public class StacksAndQueuesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
