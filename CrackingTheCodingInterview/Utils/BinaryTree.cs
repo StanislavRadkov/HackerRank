f88050cb22111c0d1039b9bf7fb0aaf1fb76dbe2
StanislavRadkov <Stanislav.vr@gmail.com>
2017-02-17 10:59:17 +0200

    Added a sample binary tree and tree node

---
+++ CrackingTheCodingInterview/Utils/BinaryTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+
+        public BinaryTree()
+        {
+
+        }
+
+        public bool IsBalanced()
+        {
+            if (Root == null)
+            {
+                return true;
+            }
+
+            var leftSubtreeHeight = Root.Left == null ? 0 : GetHeight(Root.Left);
+            var rightSubtreeHeight = Root.Right == null ? 0 : GetHeight(Root.Right);
+
+            return Math.Abs(leftSubtreeHeight - rightSubtreeHeight) <= 1;
+        }
+
+        public int GetHeight()
+        {
+            if (Root == null)
+            {
+                return 0;
+            }
+
+            return GetHeight(Root);
+        }
+
+        private int GetHeight(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(GetHeight(node.Left), GetHeight(node.Right)) + 1;
+        }
+    }
+}
