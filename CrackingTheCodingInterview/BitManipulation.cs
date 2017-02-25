45bb7c0a9c4928d0e2e9cb65c6f9e70f06e8661d
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-02-25 19:25:05 +0200

    Added solution to first binary problem

---
+++ CrackingTheCodingInterview/BitManipulation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCodingInterview
+{
+    public static class BitManipulation
+    {
+        /* Youaregiven two 32-bit numbers, N andM, andtwobitpositions, i andj. Write a
+         * method to insert M into Nsuch that M starts at bit j and ends at bit i. Youcan assume
+         * that the bits j through i have enough space to fit all ofM. That is,ifM= 10011,
+         * you can assumethat there are at least 5 bits between j and i. Youwould not, for
+         * example, havej-3 and i=2, because Mcould not fully fit between bit 3 and bit2 */
+        public static int UpdateBits(int n, int m, int i, int j)
+        {
+            for(int b = i, k = 0; b <= j; b++, k++)
+            {
+                n = (n & ~(1 << b)) | ((m & (1 << k)) << i);
+            }
+
+            return n;
+        }
+    }
+}
---
+++ CrackingTheCodingInterviewTests/BitManipulationTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CrackingTheCodingInterview;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingTheCodingInterviewTests
+{
+    [TestClass]
+    public class BitManipulationTests
+    {
+        [TestMethod]
+        public void UpdateBitsTests1()
+        {
+            Assert.AreEqual(1100, BitManipulation.UpdateBits(1024, 19, 2, 6));
+        }
+    }
+}
