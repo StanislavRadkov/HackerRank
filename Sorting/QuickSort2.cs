34f244b63ac7ec3182151b2a8d2b6115402557dc
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-01 00:21:36 +0200

    Added solution to quicksort 2

--- Sorting/QuickSort2.cs
+++ Sorting/QuickSort2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,43 @@ namespace Sorting
     // https://www.hackerrank.com/challenges/quicksort2
     public static class QuickSort2
     {
-        public static void Sort(int[] arr, int start, int end)
+        public static List<int> Sort(List<int> arr, List<List<int>> results)
         {
+            if (arr.Count <= 1)
+            {
+                return arr;
+            }
 
+            var pivot = arr[0];
+            var equal = new List<int>();
+            var left = new List<int>();
+            var right = new List<int>();
+
+            foreach (var value in arr)
+            {
+                if (value < pivot)
+                {
+                    left.Add(value);
+                }
+                else if (value > pivot)
+                {
+                    right.Add(value);
+                }
+                else
+                {
+                    equal.Add(value);
+                }
+            }
+
+            left = Sort(left, results);
+            right = Sort(right, results);
+    
+            left.AddRange(equal);
+            left.AddRange(right);
+
+            results.Add(left.ToList());
+
+            return left;
         }
     }
 }
---
+++ SortingTests/QuickSort2Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortingTests
+{
+    namespace SortingTests
+    {
+        [TestClass]
+        public class QuickSort2Tests
+        {
+            [TestMethod]
+            public void TestMethod1()
+            {
+                var data = new List<int> { 5, 8, 1, 3, 7, 9, 2 };
+                var results = new List<List<int>>();
+                QuickSort2.Sort(data, results);
+
+                var expected = new List<List<int>>
+                {
+                    new List<int> { 2, 3 },
+                    new List<int> { 1, 2, 3 },
+                    new List<int> { 7, 8, 9 },
+                    new List<int> { 1, 2, 3, 5, 7, 8, 9 },
+                };
+
+                for (var i = 0; i < results.Count; i++)
+                {
+                    CollectionAssert.AreEqual(expected[i], results[i]);
+                }
+            }
+        }
+    }
+}
