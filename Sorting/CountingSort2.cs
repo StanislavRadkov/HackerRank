89b4ad8d2b6f2123bd08e043822931e6387e1367
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-02 23:42:09 +0200

    Count 2

---
+++ Sorting/CountingSort2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    // https://www.hackerrank.com/challenges/countingsort2
+    public class CountingSort2
+    {
+        public static int[] Count(int[] arr)
+        {
+            var count = new int[100];
+
+            foreach (var value in arr)
+            {
+                count[value-1]++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ SortingTests/CountingSort2Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorting;
+
+namespace SortingTests
+{
+    [TestClass]
+    public class CountingSort2Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
