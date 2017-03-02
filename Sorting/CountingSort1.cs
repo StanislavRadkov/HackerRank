0fc533ba1b71ec391dfa902142e28faaad5c3fc4
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-02 22:37:55 +0200

    Added counting sort 1

---
+++ Sorting/CountingSort1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    // https://www.hackerrank.com/challenges/countingsort1
+    public class CountingSort1
+    {
+        public static int[] Count(int[] arr)
+        {
+            var count = new int[100];
+
+            foreach (var value in arr)
+            {
+                count[value]++;
+            }
+
+            return count;
+        }
+    }
+}
