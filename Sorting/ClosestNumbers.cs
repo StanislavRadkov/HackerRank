2e4fbbdf61c49835b239187a0f0dbeb98c40b688
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-03 14:17:41 +0200

    Added solution for closest numbers

---
+++ Sorting/ClosestNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    // https://www.hackerrank.com/challenges/closest-numbers
+    public class ClosestNumbers
+    {
+        public static List<Tuple<int, int>> GetPairs(int[] input)
+        {
+            var tuples = new List<Tuple<int, int>>();
+            var minDiff = int.MaxValue;
+
+            Array.Sort(input);
+
+            for (var i = 0; i < input.Length - 2; i++)
+            {
+                var diff = Math.Abs(input[i] - input[i + 1]);
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                    tuples.Clear();
+                    tuples.Add(new Tuple<int, int>(input[i], input[i + 1]));
+                }
+                else if (diff == minDiff)
+                {
+                    tuples.Add(new Tuple<int, int>(input[i], input[i+1]));
+                }
+            }
+
+            return tuples;
+        }
+    }
+}
---
+++ SortingTests/ClosestNumbersTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorting;
+
+namespace SortingTests
+{
+    [TestClass]
+    public class ClosestNumbersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new int[] { -20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854 };
+            var expectedOutput = new Tuple<int, int>[]
+            {
+                new Tuple<int, int>(-20, 30)
+            };
+
+            var output = ClosestNumbers.GetPairs(input);
+
+            CollectionAssert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var input = new int[] { -20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854, -520, -470 };
+            var expectedOutput = new Tuple<int, int>[]
+            {
+                new Tuple<int, int>(-520, -470),
+                new Tuple<int, int>(-20, 30)
+            };
+
+            var output = ClosestNumbers.GetPairs(input);
+            
+            CollectionAssert.AreEqual(expectedOutput, output);
+        }
+    }
+}
