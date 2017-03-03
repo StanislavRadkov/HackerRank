a557576feffeb7a89ddf2bf03535df4321fc85b2
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-03 13:10:22 +0200

    Counting sort 4 solution

---
+++ Sorting/CountingSort4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class CountingSort4
+    {
+        public static string[] Sort(KeyValuePair<int, string>[] arr)
+        {
+            var count = new int[100];
+
+            // Store count of each value
+            for (int i = 0; i < arr.Length; i++)
+            {
+                ++count[arr[i].Key];
+            }
+
+            // Change count[i] so that count[i] now contains actual
+            // position of this character in output array
+            for (var i = 1; i < count.Length; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            // Build the output string array
+            var output = new string[arr.Length];
+            for (var i = arr.Length - 1; i >= 0; i--)
+            {
+                output[count[arr[i].Key] - 1] = arr[i].Value;
+                --count[arr[i].Key];
+            }
+
+            return output;
+        }
+    }
+}
---
+++ SortingTests/CountingSort4Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorting;
+
+namespace SortingTests
+{
+    [TestClass]
+    public class CountingSort4Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "-"),
+                new KeyValuePair<int, string>(6, "-"),
+                new KeyValuePair<int, string>(0, "-"),
+                new KeyValuePair<int, string>(6, "-"),
+                new KeyValuePair<int, string>(4, "-"),
+                new KeyValuePair<int, string>(0, "-"),
+                new KeyValuePair<int, string>(6, "-"),
+                new KeyValuePair<int, string>(0, "-"),
+                new KeyValuePair<int, string>(6, "-"),
+                new KeyValuePair<int, string>(0, "-"),
+                new KeyValuePair<int, string>(4, "that"),
+                new KeyValuePair<int, string>(3, "be"),
+                new KeyValuePair<int, string>(0, "to"),
+                new KeyValuePair<int, string>(1, "be"),
+                new KeyValuePair<int, string>(5, "question"),
+                new KeyValuePair<int, string>(1, "or"),
+                new KeyValuePair<int, string>(2, "not"),
+                new KeyValuePair<int, string>(4, "is"),
+                new KeyValuePair<int, string>(2, "to"),
+                new KeyValuePair<int, string>(4, "the"),
+            };
+
+            var expectedOutput = "- - - - - to be or not to be - that is the question - - - -";
+
+            var output = CountingSort4.Sort(input);
+
+            Assert.AreEqual(expectedOutput, String.Join(" ", output));
+        }
+    }
+}
