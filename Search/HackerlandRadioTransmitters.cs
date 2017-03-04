f67f3377f66b0c8ca8fcecccd4fb985a6df125c1
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-04 20:09:14 +0200

    Added solution to hackerland radio transmitters

---
+++ Search/HackerlandRadioTransmitters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/hackerland-radio-transmitters
+    public class HackerlandRadioTransmitters
+    {
+        public static int Count(int[] data, int k)
+        {
+            Array.Sort(data);
+
+            var count = 0;
+            var pos = 0;
+
+            while (pos < data.Length)
+            {
+                count++;
+
+                var towerPosition = pos;
+                while (towerPosition < data.Length - 1 && (data[towerPosition + 1] - data[pos]) <= k)
+                {
+                    towerPosition++;
+                }
+
+                var coverage = towerPosition;
+                while (coverage < data.Length - 1 && (data[coverage + 1] - data[towerPosition]) <= k)
+                {
+                    coverage++;
+                }
+
+                pos = coverage + 1;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ SearchTests/HackerlandRadioTransmittersTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class HackerlandRadioTransmittersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(2, HackerlandRadioTransmitters.Count(input, 1));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var input = new int[] { 7, 2, 4, 6, 5, 9, 12, 11 };
+            Assert.AreEqual(3, HackerlandRadioTransmitters.Count(input, 2));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var input = new int[] { 9, 5, 4, 2, 6, 15, 12 };
+            Assert.AreEqual(4, HackerlandRadioTransmitters.Count(input, 2));
+        }
+    }
+}
