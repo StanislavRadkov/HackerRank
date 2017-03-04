68f2f1c2dd80ee26836a489f74013f0d3c2ed0b3
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-04 21:25:59 +0200

    Added icecream parlor solution

---
+++ Search/IcecreamParlor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/icecream-parlor
+    public class IcecreamParlor
+    {
+        public static Tuple<int, int> Get(int[] data, int sum)
+        {
+            var input = data
+                .Select((value, index) => new KeyValuePair<int, int>(index + 1, value))
+                .OrderBy(x => x.Value)
+                .ToArray();
+
+            var i = 0;
+            var j = input.Length - 1;
+
+            while (i < j)
+            {
+                var currentSum = input[i].Value + input[j].Value;
+
+                if (currentSum == sum)
+                {
+                    return new Tuple<int, int>(Math.Min(input[i].Key, input[j].Key), Math.Max(input[i].Key, input[j].Key));
+                }
+
+                if (currentSum > sum)
+                {
+                    j--;
+                }
+                else if (currentSum < sum)
+                {
+                    i++;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SearchTests/IcecreamParlorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class IcecreamParlorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = IcecreamParlor.Get(new int[] {1, 4, 5, 3, 2}, 4);
+            Assert.AreEqual(1, result.Item1);
+            Assert.AreEqual(4, result.Item2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = IcecreamParlor.Get(new int[] { 2, 2, 4, 3 }, 4);
+            Assert.AreEqual(1, result.Item1);
+            Assert.AreEqual(2, result.Item2);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var result = IcecreamParlor.Get(new int[] { 5, 75, 25 }, 100);
+            Assert.AreEqual(2, result.Item1);
+            Assert.AreEqual(3, result.Item2);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var result = IcecreamParlor.Get(new int[] { 150, 24, 79, 50, 88, 345, 3 }, 200);
+            Assert.AreEqual(1, result.Item1);
+            Assert.AreEqual(4, result.Item2);
+        }
+    }
+}
