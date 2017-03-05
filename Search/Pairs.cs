62d7aaab24416f87721f07ad8ae0d362c2796bda
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-05 14:02:06 +0200

    Added solution to pairs

---
+++ Search/Pairs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/pairs
+    public class Pairs
+    {
+        public static int CountPairs(int[] a, int k)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var value in a)
+            {
+                var diff = value - k;
+                if (diff <= 0)
+                {
+                    continue;
+                }
+
+                if (!dict.ContainsKey(diff))
+                {
+                    dict[diff] = 1;
+                }
+                else
+                {
+                    dict[diff]++;
+                }
+            }
+
+            var count = 0;
+
+            foreach (var value in a)
+            {
+                if (dict.ContainsKey(value))
+                {
+                    count += dict[value];
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ SearchTests/PairsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class PairsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 5, 3, 4, 2 };
+
+            Assert.AreEqual(3, Pairs.CountPairs(input, 2));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var input = new int[] { 363374326, 364147530, 61825163, 1073065718, 1281246024, 1399469912, 428047635, 491595254, 879792181, 1069262793 };
+
+            Assert.AreEqual(0, Pairs.CountPairs(input, 1));
+        }
+    }
+}
