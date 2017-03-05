c738cc564634f1520627958d357ad0c99061655a
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-05 15:42:38 +0200

    Added search

---
+++ Search/SherlockAndArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/sherlock-and-array
+    public class SherlockАndArray
+    {
+        public static bool SumsExist(int[] input)
+        {
+            if (input.Length == 1)
+            {
+                return true;
+            }
+            if (input.Length < 3)
+            {
+                return false;
+            }
+
+            var rightSum = input.Sum() - input[0];
+            var leftSum = 0;
+
+            for (var i = 1; i < input.Length - 1; i++)
+            {
+                leftSum += input[i - 1];
+                rightSum -= input[i];
+
+                if (leftSum == rightSum)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SearchTests/SherlockAndArrayTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class SherlockАndArrayTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 2, 3 };
+            Assert.AreEqual(false, SherlockАndArray.SumsExist(input));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 2, 3, 3 };
+            Assert.AreEqual(true, SherlockАndArray.SumsExist(input));
+        }
+    }
+}
