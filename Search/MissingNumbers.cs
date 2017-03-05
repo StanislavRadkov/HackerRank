11ffc0bc14de935416dfd77137a52a1dcd7f925e
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-05 13:36:05 +0200

    Added solution to missing numbers

---
+++ Search/MissingNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/missing-numbers
+    public class MissingNumbers
+    {
+        public static IEnumerable<int> Get(int[] a, int[] b)
+        {
+            var groups = from s in b
+                group s by s into g
+                select new {Stuff = g.Key, Count = g.Count()};
+
+            var dictionary = groups.ToDictionary(g => g.Stuff, g => g.Count);
+
+            foreach (var item in a)
+            {
+                dictionary[item]--;
+            }
+
+            return dictionary.Keys.OrderBy(x => x).Where(item => dictionary[item] > 0);
+        }
+    }
+}
---
+++ SearchTests/MissingNumbersTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class MissingNumbersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = new int[] { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206 };
+            var b = new int[] { 203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204 };
+
+            CollectionAssert.AreEqual(new int[] { 204, 205, 206 }, MissingNumbers.Get(a, b).ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var a = new int[] { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206 };
+            var b = new int[] { 203, 204, 204, 205, 206, 204, 207, 205, 208, 203, 206, 205, 204, 206, 204 };
+
+            CollectionAssert.AreEqual(new int[] { 204, 205, 206 }, MissingNumbers.Get(a, b).ToList());
+        }
+    }
+}
