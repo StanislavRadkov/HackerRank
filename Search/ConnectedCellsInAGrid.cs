ccce992b3a571d5e841018f41d55e6cbd08f9732
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-08 15:56:38 +0200

    Added solution to the connected grid problem

---
+++ Search/ConnectedCellsInAGrid.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/connected-cell-in-a-grid
+    public class ConnectedCellsInAGrid
+    {
+        private const int _visisted = -1;
+
+        public static int MaxCount(int[][] m)
+        {
+            int max = 0;
+
+            for (var row = 0; row < m.Length; row++)
+            {
+                for (var column = 0; column < m[row].Length; column++)
+                {
+                    max = Math.Max(max, Count(m, row, column));
+                }
+            }            
+
+            return max;
+        }
+
+        private static int Count(int[][] m, int row, int column)
+        {
+            if (!(row < m.Length && row >= 0))
+            {
+                return 0;
+            }
+
+            if (!(column < m[row].Length && column >= 0))
+            {
+                return 0;
+            }
+
+            var count = 0;
+
+            if (m[row][column] == _visisted || m[row][column] == 0)
+            {
+                return 0;
+            }
+            else if (m[row][column] == 1)
+            {
+                count++;
+                m[row][column] = _visisted;
+            }
+
+            count += Count(m, row - 1, column - 1);
+            count += Count(m, row - 1, column);
+            count += Count(m, row - 1, column + 1);
+                              
+            count += Count(m, row, column - 1);
+            count += Count(m, row, column + 1);
+                              
+            count += Count(m, row + 1, column - 1);
+            count += Count(m, row + 1, column);
+            count += Count(m, row + 1, column + 1);
+
+            return count;
+        }
+    }
+}
---
+++ SearchTests/ConnectedCellsInAGridTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class ConnectedCellsInAGridTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new int[][]
+            {
+                new int[] { 1, 1, 0, 0 },
+                new int[] { 0, 1, 1, 0 },
+                new int[] { 0, 0, 1, 0 },
+                new int[] { 1, 0, 0, 0 }
+            };
+
+            Assert.AreEqual(5, ConnectedCellsInAGrid.MaxCount(input));
+        }
+    }
+}
