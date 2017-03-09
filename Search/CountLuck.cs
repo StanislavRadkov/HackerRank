c3e86d146b28eb20c7a63a11723b331b12d98370
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-09 23:54:07 +0200

    Added solution to the count luck problem

---
+++ Search/CountLuck.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/count-luck
+    public class CountLuck
+    {
+        private const char tree = 'X';
+        private const char empty = '.';
+        private const char Hermione = 'M';
+        private const char portkey = '*';
+
+        public static bool Count(char[][] wood, int k)
+        {
+            var visited = new bool[wood.Length][];
+            for (int i = 0; i < wood.Length; i++)
+            {
+                visited[i] = new bool[wood[i].Length];
+            }
+
+            int count = 0;
+
+            for (var row = 0; row < wood.Length; row++)
+            {
+                for (var column = 0; column < wood[row].Length; column++)
+                {
+                    if (wood[row][column] == Hermione)
+                    {
+                        CountPath(wood, visited, row, column, ref count);
+
+                        return count == k;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool CountPath(char[][] wood, bool[][] visited, int row, int column, ref int count)
+        {
+            //for (var i = 0; i < wood.Length; i++)
+            //{
+            //    for (var j = 0; j < wood[i].Length; j++)
+            //    {
+            //        var prefix = (i == row && j == column) ? "[" : " ";
+            //        var postfix = (i == row && j == column) ? "]" : " ";
+            //        Trace.Write($"{prefix}{wood[i][j]}{postfix}");
+            //    }
+
+            //    Trace.WriteLine(String.Empty);
+            //}
+
+            //Trace.WriteLine(String.Empty);
+
+            Func<int, int, bool> canMove = (r, c) =>
+                (r >= 0 && r < wood.Length)
+                && (c >= 0 && c < wood[r].Length)
+                && (wood[r][c] != tree) && visited[r][c] == false;
+
+            visited[row][column] = true;
+
+            if (wood[row][column] == portkey)
+            {
+                return true;
+            }
+
+            var pathFoud = false;
+            var possibleMoves = 0;
+
+            if (canMove(row + 1, column))
+            {
+                possibleMoves++;
+                pathFoud = pathFoud || CountPath(wood, visited, row + 1, column, ref count);
+            }
+
+            if (canMove(row - 1, column))
+            {
+                possibleMoves++;
+                pathFoud = pathFoud || CountPath(wood, visited, row - 1, column, ref count);
+            }
+
+            if (canMove(row, column + 1))
+            {
+                possibleMoves++;
+                pathFoud = pathFoud || CountPath(wood, visited, row, column + 1, ref count);
+            }
+
+            if (canMove(row, column - 1))
+            {
+                possibleMoves++;
+                pathFoud = pathFoud || CountPath(wood, visited, row, column - 1, ref count);
+            }
+
+            if (possibleMoves > 1 && pathFoud)
+            {
+                count++;
+            }
+
+            return pathFoud;
+        }
+    }
+}
---
+++ SearchTests/CountLuckTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class CountLuckTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var wood = new char[][]
+            {
+                "*.M".ToCharArray(),
+                ".X.".ToCharArray()
+            };
+
+            Assert.AreEqual(true, CountLuck.Count(wood, 1));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var wood = new char[][]
+            {
+                ".X.X......X".ToCharArray(),
+                ".X*.X.XXX.X".ToCharArray(),
+                ".XX.X.XM...".ToCharArray(),
+                "......XXXX.".ToCharArray()
+            };
+
+            Assert.AreEqual(true, CountLuck.Count(wood, 3));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var wood = new char[][]
+            {
+                ".X.X......X".ToCharArray(),
+                ".X*.X.XXX.X".ToCharArray(),
+                ".XX.X.XM...".ToCharArray(),
+                "......XXXX.".ToCharArray()
+            };
+
+            Assert.AreEqual(false, CountLuck.Count(wood, 4));
+        }
+    }
+}
