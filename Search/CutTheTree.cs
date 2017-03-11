e523b0dc61b53496e941652dab0fcd54582a045a
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-11 19:33:26 +0200

    Added solution to cut the tree

---
+++ Search/CutTheTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    // https://www.hackerrank.com/challenges/cut-the-tree
+    public class Edge
+    {
+        public int value;
+        public Vertex vertex;
+        public Edge(Vertex vertex, int value)
+        {
+            this.value = value;
+            this.vertex = vertex;
+        }
+    }
+
+    public class Vertex
+    {
+        private int MinDiff;
+        private int Value;
+        private bool IsInit;
+        private List<Edge> Edges;
+
+        public Vertex(int value)
+        {
+            Value = value;
+            Edges = new List<Edge>();
+        }
+
+        public void AddEdge(Vertex vertex, int value)
+        {
+            Edges.Add(new Edge(vertex, value));
+        }
+
+        public int GetMinDiff(int sum)
+        {
+            int min = sum;
+            foreach (var edge in Edges)
+            {
+                //Get difference of both possible trees
+                int currentMin = Math.Abs(sum - 2 * edge.value);
+                //Update min
+                min = Math.Min(currentMin, min);
+            }
+
+            return min;
+        }
+
+        public int Init()
+        {
+            this.IsInit = true;
+            Edge caller = null;
+            int sum = this.Value;
+            //For each edge
+            foreach (var edge in Edges)
+            {
+                //If it's the caller, save for later
+                if (edge.vertex.IsInit)
+                {
+                    caller = edge;
+                    //Otherwise, get the sum of
+                    //all vertices for that edge
+                }
+                else
+                {
+                    sum += edge.value = edge.vertex.Init();
+                }
+            }
+            //If it's the caller
+            //subtract this vertex's
+            //sum from the tree's sum
+            if (caller != null)
+            {
+                caller.value -= sum;
+            }
+            return sum;
+        }
+    }
+}
---
+++ SearchTests/CutTheTreeTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SearchTests
+{
+    [TestClass]
+    public class CutTheTreeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var values = new[] { 100, 200, 100, 500, 100, 600 };
+            var edges = new[]
+            {
+                new[] { 1, 2 },
+                new[] { 2, 3 },
+                new[] { 2, 5 },
+                new[] { 4, 5 },
+                new[] { 5, 6 }
+            };
+
+            //Get vertex values and tree sum
+            Vertex[] verts = new Vertex[values.Length];
+            var n = 0;
+            int sum = 0;
+            foreach (var value in values)
+            {
+                sum += value;
+                verts[n++] = new Vertex(value);
+            }
+
+            foreach (var edge in edges)
+            {
+                verts[edge[0] - 1].AddEdge(verts[edge[1] - 1], sum);
+                verts[edge[1] - 1].AddEdge(verts[edge[0] - 1], sum);
+            }
+
+            //Initialize
+            verts[0].Init();
+
+            //Find smallest diff
+            int min = int.MaxValue;
+            foreach(var vertex in verts)
+            {
+                int curMin = vertex.GetMinDiff(sum);
+                //Update min
+                min = Math.Min(curMin, min);
+            }
+
+            Assert.AreEqual(400, min);
+        }
+    }
+}
