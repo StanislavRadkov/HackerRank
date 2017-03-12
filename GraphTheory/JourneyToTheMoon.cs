e1ce92073abed135b3ee0d897b33566beb9b37b3
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-12 11:51:07 +0200

    Added solution to journey to the moon problem

---
+++ GraphTheory/JourneyToTheMoon.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTheory
+{
+    // https://www.hackerrank.com/challenges/journey-to-the-moon/
+    public class JourneyToTheMoon
+    {
+        private List<HashSet<int>> _countries;
+        private int _n;
+
+        public JourneyToTheMoon(int n)
+        {
+            _n = n;
+            _countries = new List<HashSet<int>>();
+        }
+
+        public void Add(int a, int b)
+        {
+            var found = false;
+            var foundIndex = 0;
+
+            for (var i = 0; i < _countries.Count; i++)
+            {
+                var country = _countries[i];
+                if (country.Contains(a) || country.Contains(b))
+                {
+                    country.Add(a);
+                    country.Add(b);
+
+                    if (found)
+                    {
+                        _countries[i].UnionWith(_countries[foundIndex]);
+                        _countries.RemoveAt(foundIndex);
+                        break;
+                    }
+                    else
+                    {
+                        found = true;
+                        foundIndex = i;
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                var hash = new HashSet<int> {a, b};
+                _countries.Add(hash);
+            }
+        }
+
+        public long GetPossibleCombinations()
+        {
+            checked
+            {
+                long n = _n;
+                long pairs = 0;
+                long countriesCount = _countries.Count + (_n - _countries.Select(s => s.Count).Sum());
+
+                for (var j = 0; j < countriesCount; j++)
+                {
+                    if (j < _countries.Count)
+                    {
+                        pairs += (n -= _countries[j].Count)*_countries[j].Count;
+                    }
+                    else
+                    {
+                        pairs += (n -= 1)*1;
+                    }
+                }
+
+                return pairs;
+            }
+        }
+    }
+}
---
+++ GraphTheoryTests/JourneyToTheMoonTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using GraphTheory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphTheoryTests
+{
+    [TestClass]
+    public class JourneyToTheMoonTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var n = 5;
+            var k = 3;
+            var input = new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 2, 3 },
+                new int[] { 0, 4 }
+            };
+
+            var journey = new JourneyToTheMoon(n);
+            for (var i = 0; i < k; i++)
+            {
+                journey.Add(input[i][0], input[i][1]);
+            }
+
+            Assert.AreEqual(6, journey.GetPossibleCombinations());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var n = 4;
+            var k = 1;
+            var input = new int[][]
+            {
+                new int[] { 0, 2 }
+            };
+
+            var journey = new JourneyToTheMoon(n);
+            for (var i = 0; i < k; i++)
+            {
+                journey.Add(input[i][0], input[i][1]);
+            }
+
+            Assert.AreEqual(5, journey.GetPossibleCombinations());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var n = 10;
+            var k = 7;
+            var input = new int[][]
+            {
+                new[] { 0, 2 }, //1
+                new[] { 1, 8 }, //1
+                new[] { 1, 4 }, //1
+                new[] { 2, 8 }, //1
+                new[] { 2, 6 }, //1
+                new[] { 3, 5 }, //2
+                new[] { 6, 9 }  //3
+            };
+
+            var journey = new JourneyToTheMoon(n);
+            for (var i = 0; i < k; i++)
+            {
+                journey.Add(input[i][0], input[i][1]);
+            }
+
+            Assert.AreEqual(23, journey.GetPossibleCombinations());
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var n = 7;
+            var k = 3;
+            var input = new int[][]
+            {
+                new[] { 0, 1 },
+                new[] { 1, 2 },
+                new[] { 3, 4 }
+            };
+
+            var journey = new JourneyToTheMoon(n);
+            for (var i = 0; i < k; i++)
+            {
+                journey.Add(input[i][0], input[i][1]);
+            }
+
+            Assert.AreEqual(17, journey.GetPossibleCombinations());
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var n = 100000;
+            var k = 2;
+            var input = new int[][]
+            {
+                new[] { 1, 2 },
+                new[] { 3, 4 }
+            };
+
+            var journey = new JourneyToTheMoon(n);
+            for (var i = 0; i < k; i++)
+            {
+                journey.Add(input[i][0], input[i][1]);
+            }
+
+            Assert.AreEqual(4999949998L, journey.GetPossibleCombinations());
+        }
+    }
+}
