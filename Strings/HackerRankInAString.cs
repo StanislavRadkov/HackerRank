fe7c8e6003d0fa481760560c07a3b77b5f457ced
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-15 17:17:38 +0200

    Solution to the hackerrank is a string problem

---
+++ Strings/HackerRankInAString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/hackerrank-in-a-string
+    public static class HackerRankInAString
+    {
+        private static readonly string hackerrank = "hackerrank";
+
+        public static bool Is(string a)
+        {
+            var p = 0;
+            for (var i = 0; i < a.Length && p < hackerrank.Length; i++)
+            {
+                if (a[i] == hackerrank[p])
+                    p++;
+            }
+
+            return p == hackerrank.Length;
+        }
+    }
+}
---
+++ StringsTests/HackerRankInAStringTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class HackerRankInAStringTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, HackerRankInAString.Is("haacckkerrannkk"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, HackerRankInAString.Is("haacckkerannk"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(true, HackerRankInAString.Is("hereiamstackerrank"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(false, HackerRankInAString.Is("hackerworld"));
+        }
+    }
+}
