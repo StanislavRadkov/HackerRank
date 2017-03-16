a34c560bbdfff7553e67b6984e4a1fea3bde713b
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-16 11:32:02 +0200

    Added solution to mars exploration problem

---
+++ Strings/MarsExploration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/mars-exploration
+    public static class MarsExploration
+    {
+        public static string sos = "SOS";
+
+        public static int CountAlteredLetters(string k)
+        {
+            int count = 0;
+
+            for (int i = 0, p = 0; i < k.Length; i++, p = ++p % sos.Length)
+            {
+                if (k[i] != sos[p])
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ StringsTests/MarsExplorationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class MarsExplorationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, MarsExploration.CountAlteredLetters("SOSSPSSQSSOR"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, MarsExploration.CountAlteredLetters("SOSSOT"));
+        }
+    }
+}
