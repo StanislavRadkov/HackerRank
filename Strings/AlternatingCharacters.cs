b8ff9596936b9985cd2303e6ae868524ae444300
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-16 14:38:39 +0200

    Added solution to the Alternating Characters problem

---
+++ Strings/AlternatingCharacters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/alternating-characters
+    public static class AlternatingCharacters
+    {
+        public static int Count(string a)
+        {
+            var count = 0;
+            var p = 0;
+
+            for (var i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] == a[i + 1])
+                {
+                    p++;
+                }
+                else
+                {
+                    count += p;
+                    p = 0;
+                }
+            }
+
+            count += p;
+
+            return count;
+        }
+    }
+}
---
+++ StringsTests/AlternatingCharactersTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class AlternatingCharactersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, AlternatingCharacters.Count("AAAA"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(4, AlternatingCharacters.Count("BBBBB"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(0, AlternatingCharacters.Count("ABABABAB"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(0, AlternatingCharacters.Count("BABABA"));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(4, AlternatingCharacters.Count("AAABBB"));
+        }
+    }
+}
