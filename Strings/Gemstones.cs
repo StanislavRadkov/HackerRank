00227cfa0a4a6ea9cce6d62d89f4277494fc92d1
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-16 12:53:11 +0200

    Added solution to gemstones

---
+++ Strings/Gemstones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/gem-stones
+    public static class Gemstones
+    {
+        public static int Count(IEnumerable<string> a)
+        {
+            var hash = new HashSet<char>("abcdefghijklmnopqrstuvwxyz");
+
+            foreach (var s in a)
+            {
+                hash.IntersectWith(s);
+            }
+
+            return hash.Count;
+        }
+    }
+}
---
+++ StringsTests/GemstonesTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class GemstonesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new string[] 
+            {
+                "abcdde",
+                "baccd",
+                "eeabg"
+            };
+
+            Assert.AreEqual(2, Gemstones.Count(input));
+        }
+    }
+}
