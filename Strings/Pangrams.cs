b9d6b8adc189d30d948ad0274ab89567ae880dd0
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-16 13:40:47 +0200

    Added solution to the pangrams problem

---
+++ Strings/Pangrams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/pangrams
+    public static class Pangrams
+    {
+        private static readonly HashSet<char> alphabet = new HashSet<char>("abcdefghijklmnopqrstuvwxyz");
+
+        public static bool Is(string a)
+        {
+            return alphabet.Except(a.ToLower()).Count() == 0;
+        }
+    }
+}
---
+++ StringsTests/PangramsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class PangramsTests
+    {
+        [TestMethod]
+        public void TestMethod1() 
+        {
+            Assert.AreEqual(true, Pangrams.Is("The quick brown fox jumps over the lazy dog"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(true, Pangrams.Is("We promptly judged antique ivory buckles for the next prize"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(false, Pangrams.Is("We promptly judged antique ivory buckles for the prize"));
+        }
+    }
+}
