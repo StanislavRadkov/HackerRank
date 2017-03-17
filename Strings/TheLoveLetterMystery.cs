a5a70851cbbc75ca56900cf8a0b05d5861d946ed
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-17 11:33:36 +0200

    Added solution to the love letter problem

---
+++ Strings/TheLoveLetterMystery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/the-love-letter-mystery
+    public static class TheLoveLetterMystery
+    {
+        public static int Count(string a)
+        {
+            var count = 0;
+            for (var i = 0; i < a.Length/2; i++)
+            {
+                count += Math.Abs(a[i] - a[a.Length - i - 1]);
+            }
+
+            return count;
+        }
+    }
+}
---
+++ StringsTests/TheLoveLetterMysteryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class TheLoveLetterMysteryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, TheLoveLetterMystery.Count("abc"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(0, TheLoveLetterMystery.Count("abcba"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(4, TheLoveLetterMystery.Count("abcd"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(2, TheLoveLetterMystery.Count("cba"));
+        }
+    }
+}
