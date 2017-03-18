0a4af38f9add318bf02a89e6234b93a1d2b455b8
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-18 17:33:28 +0200

    Added solution to two characters problem

---
+++ Strings/TwoCharacters.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/two-characters
+    public static class TwoCharacters
+    {
+        public static int CountMax(string a)
+        {
+            if (a.Length <= 1)
+            {
+                return 0;
+            }
+
+            const int numerOfLetters = 26;
+
+            var pairs = new int[numerOfLetters, numerOfLetters];
+            var count = new int[numerOfLetters, numerOfLetters];
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                var letter = (int) a[i];
+                var num = (int) a[i] -'a';
+
+                for (var col = 0; col < numerOfLetters; col++)
+                {
+                    if (pairs[num, col] == letter)
+                    {
+                        count[num, col] = -1;
+                    }
+
+                    if (count[num, col] != -1)
+                    {
+                        pairs[num, col] = letter;
+                        count[num, col]++;
+                    }
+                }
+
+                for (var row = 0; row < numerOfLetters; row++)
+                {
+                    if (pairs[row, num] == letter)
+                    {
+                        count[row, num] = -1;
+                    }
+
+                    if (count[row, num] != -1)
+                    {
+                        pairs[row, num] = letter;
+                        count[row, num]++;
+                    }
+                }
+            }
+
+            return Math.Max(0, count.Cast<int>().Max());
+        }
+    }
+}
---
+++ StringsTests/TwoCharactersTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class TwoCharactersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, TwoCharacters.CountMax("beabeefeab"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, TwoCharacters.CountMax("ab"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(0, TwoCharacters.CountMax("uyetuppelecblwipdsqabzsvyfaezeqhpnalahnpkdbhzjglcuqfjnzpmbwprelbayyzovkhacgrglrdpmvaexkgertilnfooeazvulykxypsxicofnbyivkthovpjzhpohdhuebazlukfhaavfsssuupbyjqdxwwqlicbjirirspqhxomjdzswtsogugmbnslcalcfaxqmionsxdgpkotffycphsewyqvhqcwlufekxwoiudxjixchfqlavjwhaennkmfsdhigyeifnoskjbzgzggsmshdhzagpznkbahixqgrdnmlzogprctjggsujmqzqknvcuvdinesbwpirfasnvfjqceyrkknyvdritcfyowsgfrevzon"));
+        }
+    }
+}
