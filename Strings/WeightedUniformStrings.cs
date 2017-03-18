ba73fef9fa4c7bcf50c3be703926eda28b4208ce
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-18 17:57:14 +0200

    Added Weighted uniform strings solution

---
+++ Strings/WeightedUniformStrings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/weighted-uniform-string
+    public class WeightedUniformStrings
+    {
+        private HashSet<int> _values;
+        private string _a;
+         
+        public WeightedUniformStrings(string a)
+        {
+            _values = new HashSet<int>();
+
+            _a = a;
+        }
+
+        public void Init()
+        {
+            _values.Add(_a[0] - 96);
+            var currentSubstringValue = (int)(_a[0] - 96);
+
+            for (var i = 1; i < _a.Length; i++)
+            {
+
+                if (_a[i] != _a[i - 1])
+                {
+                    _values.Add(currentSubstringValue);
+                    currentSubstringValue = _a[i] - 96;
+                }
+                else
+                {
+                    _values.Add(currentSubstringValue);
+                    currentSubstringValue += (_a[i] - 96);
+                }
+
+                _values.Add(_a[i] - 96);
+            }
+
+            _values.Add(currentSubstringValue);
+        }
+
+        public bool In(int value)
+        {
+            return _values.Contains(value);
+        }
+    }
+}
---
+++ StringsTests/WeightedUniformStringsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class WeightedUniformStringsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var w = new WeightedUniformStrings("abccddde");
+
+            w.Init();
+
+            Assert.AreEqual(true, w.In(1));
+            Assert.AreEqual(true, w.In(3));
+            Assert.AreEqual(true, w.In(12));
+            Assert.AreEqual(true, w.In(5));
+            Assert.AreEqual(false, w.In(9));
+            Assert.AreEqual(false, w.In(10));
+        }
+    }
+}
