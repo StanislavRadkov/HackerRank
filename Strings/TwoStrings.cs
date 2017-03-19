d513b0a5a8d51ee8e7f35fa65c469b0053003d66
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-19 11:35:07 +0200

    Added solution to two strings

---
+++ Strings/TwoStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/two-strings
+    public static class TwoStrings
+    {
+        public static bool HaveSubstrings(string a, string b)
+        {
+            var hashA = new HashSet<char>(a.ToLower());
+
+            return hashA.Intersect(b.ToLower()).Any();
+        }
+    }
+}
---
+++ StringsTests/TwoStringsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class TwoStringsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, TwoStrings.HaveSubstrings("hello", "world"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, TwoStrings.HaveSubstrings("hi", "world"));
+        }
+    }
+}
