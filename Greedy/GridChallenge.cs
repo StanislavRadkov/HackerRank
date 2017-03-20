9ed289478edf5abcd4af2f22319fc77a375438d0
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-20 21:29:44 +0200

    Added solution to the grid challenge

---
+++ Greedy/GridChallenge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/grid-challenge
+    public class GridChallenge
+    {
+        private char[] _previousLine;
+
+        public bool Line(string input)
+        {
+            var line = input.ToCharArray();
+            Array.Sort(line);
+
+            if (_previousLine != null)
+            {
+                for (var i = 0; i < _previousLine.Length; i++)
+                {
+                    if (_previousLine[i] > line[i])
+                        return false;
+                }
+            }
+
+            _previousLine = line;
+
+            return true;
+        }
+    }
+}
---
+++ GreedyTests/GridChallengeTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Greedy;
+
+namespace GreedyTests
+{
+    [TestClass]
+    public class GridChallengeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var grid = new GridChallenge();
+            Assert.AreEqual(true, grid.Line("ebacd"));
+            Assert.AreEqual(true, grid.Line("fghij"));
+            Assert.AreEqual(true, grid.Line("olmkn"));
+            Assert.AreEqual(true, grid.Line("trpqs"));
+            Assert.AreEqual(true, grid.Line("xywuv"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var grid = new GridChallenge();
+            Assert.AreEqual(true, grid.Line("ppp"));
+            Assert.AreEqual(true, grid.Line("ypp"));
+            Assert.AreEqual(true, grid.Line("wyw"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var grid = new GridChallenge();
+            Assert.AreEqual(true, grid.Line("lyivr"));
+            Assert.AreEqual(false, grid.Line("jgfew"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var grid = new GridChallenge();
+            Assert.AreEqual(true, grid.Line("1"));
+        }
+    }
+}
