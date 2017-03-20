ab1afba7234403abfac44a4f361c08233b1d3da6
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-20 20:47:51 +0200

    Added solution to the Marcs Cakewalk

---
+++ Greedy/MarcsCakewalk.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/marcs-cakewalk
+    public static class MarcsCakewalk
+    {
+        public static long Miles(int[] cakes)
+        {
+            long result = 0;
+
+            Array.Sort(cakes, (i1, i2) => i2.CompareTo(i1));
+
+            for (var i = 0; i < cakes.Length; i++)
+            {
+                result += (long)Math.Pow(2, i) * cakes[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GreedyTests/MarcsCakewalkTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Greedy;
+
+namespace GreedyTests
+{
+    [TestClass]
+    public class MarcsCakewalkTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(11, MarcsCakewalk.Miles(new[] { 1, 3, 2 }));
+        }
+    }
+}
