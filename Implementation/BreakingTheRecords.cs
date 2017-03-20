58282dd77d190d9f7cec7cef1ca1e7fb93a02e8a
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-20 11:06:12 +0200

    Added solution to breaking the records

---
+++ Implementation/BreakingTheRecords.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation
+{
+    // https://www.hackerrank.com/challenges/breaking-best-and-worst-records
+    public static class BreakingTheRecords
+    {
+        public static Tuple<int, int> Count(int[] input)
+        {
+            var min = input[0];
+            var max = input[0];
+            var minCount = 0;
+            var maxCount = 0;
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                var value = input[i];
+                if (value < min)
+                {
+                    min = value;
+                    minCount++;
+                }
+                else if (value > max)
+                {
+                    max = value;
+                    maxCount++;
+                }
+            }
+
+            return new Tuple<int, int>(maxCount, minCount);
+        }
+    }
+}
---
+++ ImplementationTests/BreakingTheRecordsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImplementationTests
+{
+    [TestClass]
+    public class BreakingTheRecordsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = BreakingTheRecords.Count(new int[] { 10, 5, 20, 20, 4, 5, 2, 25, 1 });
+            Assert.AreEqual(2, result.Item1);
+            Assert.AreEqual(4, result.Item2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = BreakingTheRecords.Count(new int[] { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 });
+            Assert.AreEqual(4, result.Item1);
+            Assert.AreEqual(0, result.Item2);
+        }
+    }
+}
