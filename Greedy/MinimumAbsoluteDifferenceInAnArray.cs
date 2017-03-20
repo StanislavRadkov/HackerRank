c618f2b3ad5a5fc191468690f65bf0b273f8888e
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-20 11:29:09 +0200

    Added greedy project and solution to a minimum absolute difference in an array

---
+++ Greedy/MinimumAbsoluteDifferenceInAnArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array
+    public static class MinimumAbsoluteDifferenceInAnArray
+    {
+        public static int Get(int[] input)
+        {
+            Array.Sort(input);
+
+            var min = int.MaxValue;
+            for (var i = 1; i < input.Length; i++)
+            {
+                var result = Math.Abs(input[i] - input[i - 1]);
+                if (result < min)
+                {
+                    min = result;
+                } 
+            }
+
+            return min;
+        }
+    }
+}
---
+++ GreedyTests/MinimumAbsoluteDifferenceInAnArrayTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Greedy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MinimumAbsoluteDifferenceInAnArrayTests
+{
+    [TestClass]
+    public class MinimumAbsoluteDifferenceInAnArrayTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, MinimumAbsoluteDifferenceInAnArray.Get(new int[] { 3, -7, 0 }));
+        }
+    }
+}
