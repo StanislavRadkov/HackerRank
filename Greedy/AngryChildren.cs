08c733206217dafc8e8caa1391ab7b0e8d5619d5
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-21 22:05:15 +0200

    Added solution to the angry children problem

---
+++ Greedy/AngryChildren.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/angry-children
+    public static class AngryChildren
+    {
+        public static int GetMinUnfairness(int[] values, int k)
+        {
+            Array.Sort(values);
+            var min = int.MaxValue;
+
+            for (var i = k - 1; i < values.Length; i++)
+            {
+                var diff = values[i] - values[i - (k - 1)];
+
+                if (diff < min)
+                {
+                    min = diff;
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ GreedyTests/AngryChildrenTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greedy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GreedyTests
+{
+    [TestClass]
+    public class AngryChildrenTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(20, AngryChildren.GetMinUnfairness(new int[] { 10, 100, 300, 200, 1000, 20, 30 }, 3));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(3, AngryChildren.GetMinUnfairness(new int[] { 1, 2, 3, 4, 10, 20, 30, 40, 100, 200 }, 4));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(2, AngryChildren.GetMinUnfairness(new int[] { 10, 20, 30, 100, 101, 102 }, 3));
+        }
+    }
+}
