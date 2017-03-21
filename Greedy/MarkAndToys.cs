0db3d44398927b27bbc44172d37e19cd0e0444c4
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-21 10:24:52 +0200

    mark and toys

---
+++ Greedy/MarkAndToys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/mark-and-toys
+    public static class MarkAndToys
+    {
+        public static int Max(int[] toys, int money)
+        {
+            Array.Sort(toys);
+
+            return toys.TakeWhile(x => (money -= x) > 0).Count();                
+        }
+    }
+}
