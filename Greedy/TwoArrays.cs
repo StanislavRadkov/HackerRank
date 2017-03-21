08b498caa0a2df69ed306a775958c99ffce7505f
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-21 22:43:44 +0200

    Added solution to two arrays

---
+++ Greedy/TwoArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/two-arrays
+    public static class TwoArrays
+    {
+        public static bool PermutationExists(int[] a, int[] b, int k)
+        {
+            var c = new int[a.Length + b.Length];
+
+            a.CopyTo(c, 0);
+            b.CopyTo(c, b.Length);
+
+            Array.Sort(c);
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                if ((c[i] + c[c.Length - i - 1]) < k)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ GreedyTests/TwoArraysTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greedy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GreedyTests
+{
+    [TestClass]
+    public class TwoArraysTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, TwoArrays.PermutationExists(new int[] {2, 1, 3 }, new int[] { 7, 8, 9}, 10));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, TwoArrays.PermutationExists(new int[] { 1, 2, 2, 1 }, new int[] { 3, 3, 3, 4 }, 5));
+        }
+    }
+}
