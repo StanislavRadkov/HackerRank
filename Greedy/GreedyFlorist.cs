cbc067c8d3e5a6298cccfa147f7f65c56e6b8814
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-21 21:36:25 +0200

    Added greedy florist solution

---
+++ Greedy/GreedyFlorist.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/greedy-florist
+    public static class GreedyFlorist
+    {
+        public static int GetPrice(int[] prices, int flowers, int people)
+        {
+            var price = 0;
+            Array.Sort(prices, (x1, x2) => x2.CompareTo(x1));
+
+            var p = 0;
+            var remainingPeopleForIteration = people;
+
+            for (var i = 0; i < prices.Length && flowers > 0; i = (i+1) % prices.Length)
+            {
+                price += (p + 1) * prices[i];
+                flowers--;
+                remainingPeopleForIteration--;
+
+                if (remainingPeopleForIteration == 0)
+                {
+                    p++;
+                    remainingPeopleForIteration = people;
+                }
+            }
+
+            return price;
+        }
+    }
+}
---
+++ GreedyTests/GreedyFloristTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greedy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GreedyTests
+{
+    [TestClass]
+    public class GreedyFloristTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(13, GreedyFlorist.GetPrice(new int[] { 2, 5, 6 }, 3, 3));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(15, GreedyFlorist.GetPrice(new int[] { 2, 5, 6 }, 3, 2));
+        }
+    }
+}
