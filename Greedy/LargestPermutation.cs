5a0f76a0081ff73811271d1f2252824bd06ebb4e
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-21 11:59:18 +0200

    Added solution to the largest permutation

---
+++ Greedy/LargestPermutation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/largest-permutation
+    public static class LargestPermutation
+    {
+        public static void Mutate(int[] a, int k)
+        {
+            var dict = new Dictionary<int, int>(a.Length);
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                dict[a[i]] = i;
+            }
+
+            Action<int, int> swap = (x, y) =>
+            {
+                dict[a[x]] = y;
+                dict[a[y]] = x;
+
+                var temp = a[x];
+                a[x] = a[y];
+                a[y] = temp;
+            };
+
+            for (var i = 0; i < a.Length && k > 0; i++)
+            {
+                var expected = a.Length - i;
+                if (a[i] != expected)
+                {
+                    swap(i, dict[expected]);
+                    --k;
+                }
+            }
+        }
+    }
+}
