cedbcd0afe54d8f8061a68d46dc687e6726640bc
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-22 11:41:33 +0200

    Added solution to the jim and the orders problem

---
+++ Greedy/JimAndTheOrders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    // https://www.hackerrank.com/challenges/jim-and-the-orders
+    public static class JimAndTheOrders
+    {
+        public static IEnumerable<int> Orders(KeyValuePair<int, int>[] orders)
+        {
+            return orders
+                .OrderBy(x => x.Value)
+                .ThenBy(x => x.Key)
+                .Select(x => x.Key);
+        }
+    }
+}
---
+++ GreedyTests/JimAndTheOrdersTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Greedy;
+
+namespace GreedyTests
+{
+    [TestClass]
+    public class JimAndTheOrdersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new KeyValuePair<int, int>[]
+            {
+                new KeyValuePair<int, int>(1, 3),
+                new KeyValuePair<int, int>(2, 3),
+                new KeyValuePair<int, int>(3, 3),
+            };
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3 }, JimAndTheOrders.Orders(input).ToArray());
+        }
+    }
+}
