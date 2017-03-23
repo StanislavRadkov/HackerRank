23b550ced2b791377462ccc193949e6b47871918
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-23 08:39:31 +0200

    Added solution to the sansa and xor problem

---
+++ BitManipulation/SansaAndXOR.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitManipulation
+{
+    // https://www.hackerrank.com/challenges/sansa-and-xor/forum
+    public static class SansaAndXOR
+    {
+        public static int XOR(int[] a)
+        {
+            // The xor of array with even length is always 0.
+            if (a.Length%2 == 0)
+            {
+                return 0;
+            }
+
+            var result = 0;
+
+            // when it is even the number appears odd times and hence will be considered in the xor sum
+            for (var i = 0; i < a.Length; i += 2)
+            {
+                result ^= a[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BitManipulationTests/SansaAndXORTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BitManipulation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitManipulationTests
+{
+    [TestClass]
+    public class SansaAndXORTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, SansaAndXOR.XOR(new int[] { 1, 2, 3 }));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(0, SansaAndXOR.XOR(new int[] { 4, 5, 7, 5 }));
+        }
+    }
+}
