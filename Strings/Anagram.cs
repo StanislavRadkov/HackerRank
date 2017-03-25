274b7828303ea6e2adc39a4ac247198189437237
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-25 15:21:48 +0200

    Added solution to the anagram problem

---
+++ Strings/Anagram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/anagram
+    public static class Anagram
+    {
+        public static int Changes(string a)
+        {
+            if (a.Length % 2 != 0)
+            {
+                return -1;
+            }
+
+            var letters = new int[26];
+
+            for (var i = a.Length - 1; i >= a.Length / 2; i--)
+            {
+                letters[a[i] - 'a']++;
+            }
+
+            for (var i = 0; i < a.Length / 2; i++)
+            {
+                if (letters[a[i] - 'a'] > 0)
+                {
+                    letters[a[i] - 'a']--;
+                }
+            }
+
+            return letters.Sum();
+        }
+    }
+}
---
+++ StringsTests/AnagramTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class AnagramTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, Anagram.Changes("aaabbb"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, Anagram.Changes("ab"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(-1, Anagram.Changes("abc"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(2, Anagram.Changes("mnop"));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(0, Anagram.Changes("xyyx"));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(1, Anagram.Changes("xaxbbbxx"));
+        }
+    }
+}
