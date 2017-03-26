b2a529b9662fcac7b3052275c44333b54cf1a63d
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-26 18:49:11 +0300

    Added solution to string construction

---
+++ Strings/StringConstruction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/string-construction
+    public static class StringConstruction
+    {
+        public static int Count(string a)
+        {
+            var hash = new HashSet<char>(a.ToCharArray());
+            return hash.Count;
+        }
+    }
+}
