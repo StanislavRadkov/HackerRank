d27175f1048264919fce285e6cbcf8ad0f119f5d
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-03-26 20:04:29 +0300

    Added sherlock problem, not all tests are passing

---
+++ Strings/SherlockAndValidString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/sherlock-and-valid-string
+    public static class SherlockAndValidString
+    {
+        public static bool CanBeConverted(string a)
+        {
+            int[] count = new int[26];
+
+            int max = int.MinValue;
+            for (var i = 0; i < a.Length; i++)
+            {
+                var index = a[i] - 'a';
+                count[index]++;
+                if (count[index] > max)
+                {
+                    max = count[index];
+                }
+            }
+
+            var diff = 0;
+            for (var i = 0; i < count.Length; i++)
+            {
+                if(count[i] > 0)
+                    diff += Math.Abs(max - count[i]);
+            }
+
+            return diff <= 1;
+        }
+    }
+}
---
+++ StringsTests/SherlockAndValidStringTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class SherlockAndValidStringTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(false, SherlockAndValidString.CanBeConverted("aabbcd"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(true, SherlockAndValidString.CanBeConverted("jtqgugmcsxvdwidtcyqpogkdifapuloqykjfxruvfrshcehekoiwbpbrprahwvhliglyxynjotbaswnnnmxbkmcftvsdqajemeul"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(true, SherlockAndValidString.CanBeConverted("ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"));
+        }
+    }
+}
