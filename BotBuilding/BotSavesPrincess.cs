5f8f82307d97ee8ae5ce3be504edc88959d4a05c
StanislavRadkov <Stanislav.vr@gmail.com>
2017-03-29 12:25:39 +0300

    Added solution to bot saves princes problem

---
+++ BotBuilding/BotSavesPrincess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotBuilding
+{
+    // https://www.hackerrank.com/challenges/saveprincess
+    public static class BotSavesPrincess
+    {
+        public static List<Tuple<string, int>> DisplayPathtoPrincess(Tuple<int, int> m, Tuple<int, int> p)
+        {
+            var moves = new List<Tuple<string, int>>();
+
+            if (m.Item1 > p.Item1)
+            {
+                moves.Add(new Tuple<string, int>("UP", m.Item1 - p.Item1));
+            }
+            else if (m.Item1 < p.Item1)
+            {
+                moves.Add(new Tuple<string, int>("DOWN", p.Item1 - m.Item1));
+            }
+
+            if (m.Item2 > p.Item2)
+            {
+                moves.Add(new Tuple<string, int>("LEFT", m.Item2 - p.Item2));
+            }
+            else if (m.Item2 < p.Item2)
+            {
+                moves.Add(new Tuple<string, int>("RIGHT", p.Item2 - m.Item2));
+            }
+
+            return moves;
+        }
+    }
+}
---
+++ BotBuildingTests/BotSavesPrincessTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BotBuilding;
+using System.Collections.Generic;
+
+namespace BotBuildingTests
+{
+    [TestClass]
+    public class BotSavesPrincessTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expectedOutput = new List<Tuple<String, int>>()
+            {
+                new Tuple<string, int>("DOWN", 1),
+                new Tuple<string, int>("LEFT", 1),
+            };
+
+            CollectionAssert.AreEqual(expectedOutput, BotSavesPrincess.DisplayPathtoPrincess(new Tuple<int, int>(1, 1), new Tuple<int, int>(2, 0)));
+        }
+    }
+}
