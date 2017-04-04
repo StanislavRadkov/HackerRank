c754f50cf08e844e031564bf8e5c15e97a75eb81
StanislavRadkov <Stanislav.vr@gmail.com>
2017-04-04 12:33:45 +0300

    Added solution to game of thrones 1

---
+++ Strings/GameOfThronesI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // https://www.hackerrank.com/challenges/game-of-thrones
+    public static class GameOfThronesI
+    {
+        public static bool Validate(string a)
+        {
+            var letters = new int[26];
+
+            foreach (var c in a)
+            {
+                letters[c - 'a']++;
+            }
+
+            return letters.Count(c => c % 2 != 0) <= 1;
+        }
+    }
+}
---
+++ StringsTests/GameOfThronesITests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsTests
+{
+    [TestClass]
+    public class GameOfThronesITests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, GameOfThronesI.Validate("aaabbbb"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, GameOfThronesI.Validate("cdefghmnopqrstuvw"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(true, GameOfThronesI.Validate("cdcdcdcdeeeef"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(true, GameOfThronesI.Validate("a"));
+        }
+    }
+}
