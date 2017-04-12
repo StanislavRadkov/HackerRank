1eb38fa90d2778032807c5dadfe0ae5552765b68
StanislavRadkov <Stanislav.vr@gmail.com>
2017-04-12 14:21:01 +0300

    Added solution to the PADS problem

---
+++ SQL/AdvancedSelect/ThePADS.sql
@@ -0,0 +1,11 @@
+﻿SELECT Name + '(' + LEFT(Occupation , 1) + ')' 
+FROM Occupations
+ORDER BY Name
+
+DECLARE @a AS VARCHAR(50);
+SET @a = 'There are total ';
+
+SELECT @a + CONVERT(varchar(10), COUNT(*)) + ' ' + LOWER(Occupation) + 's.'
+FROM Occupations
+GROUP BY Occupation    
+ORDER BY COUNT(*) ASC, Occupation ASC
